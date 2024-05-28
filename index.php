2091d8c71000f7c3f79f1712ccd3f0f8d8015854
ahmadmuqri0 <muqrisuhaimi25@gmail.com>
2024-05-28 12:30:41 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $number = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>this is number <?php echo $number ?></p>
+</body>
+</html>
